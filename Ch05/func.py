c5da9f664ed3ce69d0133452078456c4bd16a3b9
jjerry-k <jaeyeol2931@gmail.com>
2022-02-03 01:11:31 +0900

    Update

--- Ch05/func.py
+++ Ch05/func.py
@@ -1,9 +1,22 @@
 import numpy as np
 
-def edge_filter()
-- Roberts & Prewitt & Sobel
-- Magnitude & Gradient
-- LOG filter
-- Canny edge
-- Color edge
-- Hough transform
+def edge_filter(img, filter):
+    return edge_img
+
+def get_magnitude(img):
+    return mag
+
+def get_gradient(img):
+    return grad
+
+def logfilter(img):
+    return edge_img
+
+def canny_edge(img):
+    return edge_img
+
+def color_edge(img):
+    return edge_img
+
+def hough_transform(img):
+    return edge_img
